e266ab0aacb1faeb175962e63e6e5c50c52f6453
小鸽子 <2499349047@qq.com>
2024-05-20 22:18:17 +0800

    Site updated: 2024-05-20 22:18:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/hello-world/","2024/05/19/two/","2024/05/20/imgTest/","2024/05/17/First-md/","2024/05/20/Spring/"];function toRandomPost(){
+var posts=["2024/05/17/First-md/","2024/05/17/hello-world/","2024/05/20/imgTest/","2024/05/19/two/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
