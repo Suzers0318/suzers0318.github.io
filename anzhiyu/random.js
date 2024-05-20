07390dd2df2749fadeddcede802d3ec7d1e85f27
小鸽子 <2499349047@qq.com>
2024-05-20 14:58:03 +0800

    Site updated: 2024-05-20 14:58:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/hello-world/","2024/05/19/two/","2024/05/17/First-md/"];function toRandomPost(){
+var posts=["2024/05/17/hello-world/","2024/05/17/First-md/","2024/05/19/two/","2024/05/20/2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
