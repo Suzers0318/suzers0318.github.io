9db9b762d770119ad871156ac00f54a7f84903ff
小鸽子 <2499349047@qq.com>
2024-05-21 19:40:42 +0800

    Site updated: 2024-05-21 19:40:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/20/Spring/"];function toRandomPost(){
+var posts=["2024/05/20/Spring/","2024/05/21/Study-01/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
